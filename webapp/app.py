a7cd940e9d8d76eb25c17f0e01a5b9d31023fbd2
Sammriddh Gupta <72302725+SammriddhGupta@users.noreply.github.com>
2023-05-15 19:59:50 +1000

    Use production WSGI server - waitress

--- webapp/app.py
+++ webapp/app.py
@@ -33,4 +33,6 @@ def home():
     return render_template('index.html', states=states, prediction=prediction, form_dict=form_dict)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=1234, debug=True)
+    #app.run(host='0.0.0.0',port=1234, debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
